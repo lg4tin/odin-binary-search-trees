8cd0eb44353e61dd9c7da729b080c4929914b0b4
lg4tin <lg4tin@gmail.com>
2023-10-25 15:53:20 -0400

    Add insert func

--- script.js
+++ script.js
@@ -23,6 +23,33 @@ class Tree {
     return root;
   }
 
+  insert(value) {
+    let newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+  
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+
+          currentNode = currentNode.right;
+        }
+      }
+      
+    }
+  }
   
 }
 
